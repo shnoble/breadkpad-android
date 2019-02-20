ebdd7f6bbdd106349dd75dfe3d7381d3417f2d1d
홍석현/TOAST클라이언트개발팀/NE <shhong@nhnent.com>
2019-02-20 19:17:05 +0900

    Move the breakpad source to the app module for testing

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,45 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_daya_android_breakpad_MainActivity_stringFromJNI(
+#include "client/linux/handler/exception_handler.h"
+#include "client/linux/handler/minidump_descriptor.h"
+
+#ifdef NDEBUG
+#define DEBUG(...)
+#else
+#define DEBUG(...) __android_log_print(ANDROID_LOG_DEBUG, __VA_ARGS__)
+#endif
+
+static google_breakpad::ExceptionHandler* exceptionHandler;
+bool DumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
+                  void* context,
+                  bool succeeded) {
+    DEBUG("breakpad-android", "Dump path: %s\n", descriptor.path());
+    return succeeded;
+}
+
+
+void Crash() {
+    volatile int* a = reinterpret_cast<volatile int*>(NULL);
+    *a = 1;
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_daya_android_breakpad_MainActivity_initialize(
+        JNIEnv* env,
+        jobject obj,
+        jstring filepath) {
+    DEBUG("breakpad-android", "initialize call");
+    const char *path = env->GetStringUTFChars(filepath, 0);
+    google_breakpad::MinidumpDescriptor descriptor(path);
+    exceptionHandler = new google_breakpad::ExceptionHandler(descriptor, nullptr, DumpCallback, nullptr, true, -1);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_daya_android_breakpad_MainActivity_crash(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    DEBUG("breakpad-android", "crash call");
+    Crash();
 }
